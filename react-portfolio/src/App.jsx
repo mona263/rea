2249417c1707608be9bacb0035d2efa7c4cae2a9
Mona Abdelazeem <monaabdelazeem263.com>
2025-11-19 16:35:00 +0200

    Fix build: successful vite build

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -14,6 +14,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
+    <HashRouter>
     <MotionConfig reducedMotion="user">
       <div className="App">
         <BackgroundAnimation />
@@ -30,6 +31,7 @@ function App() {
         <Footer />
       </div>
     </MotionConfig>
+    </HashRouter>
   );
 }
 
--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  server: {
+    port: 3000
+  }
 })
